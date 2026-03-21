ae9f4aa5890c7b97b3c150f484f90fa77f6e9d51
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-21 14:19:14 -0300

    Create mute.ts

---
+++ src/commands/mute.ts
@@ -0,0 +1,35 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('mute')
+    .setDescription('Mutar um membro')
+    .addUserOption(option =>
+      option.setName('usuario')
+        .setDescription('Usuário a ser mutado')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option.setName('tempo')
+        .setDescription('Tempo em minutos')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser('usuario');
+    const tempo = interaction.options.getInteger('tempo');
+
+    const member = await interaction.guild.members.fetch(user.id).catch(() => null);
+
+    if (!member) {
+      return interaction.reply({ content: 'Usuário não encontrado.', ephemeral: true });
+    }
+
+    const ms = tempo * 60 * 1000;
+
+    await member.timeout(ms);
+
+    await interaction.reply(`🔇 ${user.tag} foi mutado por ${tempo} minutos.`);
+  }
+};
