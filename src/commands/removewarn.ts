cfb0b1ea5f8ad3603bdac6dd3fee53cbed04f1ea
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-21 18:55:47 -0300

    Create removewarn.ts

---
+++ src/commands/removewarn.ts
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { readDB, writeDB } from '../utils/database';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('removewarn')
+    .setDescription('Remover avisos de um usuário')
+    .addUserOption(option =>
+      option.setName('usuario')
+        .setDescription('Usuário')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser('usuario');
+    const db = readDB();
+    const guildId = interaction.guild.id;
+
+    if (db.warns[guildId] && db.warns[guildId][user.id]) {
+      db.warns[guildId][user.id] = [];
+    }
+
+    writeDB(db);
+
+    await interaction.reply(`🧹 Avisos de ${user.tag} foram removidos.`);
+  }
+};
