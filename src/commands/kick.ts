5a5866d3c70a8597e9f715c08c570e8ce5a004cf
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-21 16:05:28 -0300

    Update kick.ts

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
-import { sendLog } from '../utils/logs.ts';
+import { sendLog } from '../utils/logs';
 
 export default {
   data: new SlashCommandBuilder()
