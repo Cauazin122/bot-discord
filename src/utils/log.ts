4fe237caa84d8f4781f174f0ca3c3dc725e499e5
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-21 16:08:06 -0300

    Update log.ts

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,8 +1,8 @@
 import { EmbedBuilder } from 'discord.js';
 
 const logChannels = {
-  "SERVIDOR_REAL_ID": "1483071071627378718",
-  "SERVIDOR_TESTE_ID": "1463602167460921547"
+  "1483071071627378718": "1483071071627378718",
+  "1463602167460921547": "1463602167460921547"
 };
 
 export async function sendLog(guild, data) {
