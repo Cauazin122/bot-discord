e60578d74271245228ad5f57b1628d87572ee1ae
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-22 15:47:37 -0300

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,16 +1,17 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
-import * as ping from "./ping.js";
-import * as ajuda from "./help.js";
-import * as eco from "./echo.js";
-import * as info from "./info.js";
-import * as ticket from "./ticket.js";
-import * as avaliacoes from "./avaliacoes.js";
-import * as top from "./top.js";
+
+import ping from "./ping.js";
+import ajuda from "./help.js";
+import eco from "./echo.js";
+import info from "./info.js";
+import ticket from "./ticket.js";
+import avaliacoes from "./avaliacoes.js";
+import top from "./top.js";
 import ban from "./ban.js";
 import kick from "./kick.js";
 import mute from "./mute.js";
 import warn from "./warn.js";
-import * as helpadm from "./helpadm.js";
+import helpadm from "./helpadm.js";
 import removewarn from "./removewarn.js";
 import warns from "./warns.js";
 import antilink from "./antilink.js";
