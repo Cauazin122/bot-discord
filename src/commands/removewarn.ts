fdef4ea0dc192858f0ab3dde3c39905b4e6b9378
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-22 15:36:35 -0300

    Update removewarn.ts

--- src/commands/removewarn.ts
+++ src/commands/removewarn.ts
@@ -1,42 +1,44 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
-import { readDB, writeDB } from '../utils/database';
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} from 'discord.js';
+
+import { readDB, writeDB } from '../utils/database.js';
 
 export default {
   data: new SlashCommandBuilder()
     .setName('removewarn')
-    .setDescription('Remover avisos de um usuário')
+    .setDescription('Remove warns de um usuário')
     .addUserOption(option =>
       option.setName('usuario')
         .setDescription('Usuário')
         .setRequired(true)
     )
     .addIntegerOption(option =>
-      option.setName('numero')
-    .setDescription('Número do warn (opcional)')
-    .setRequired(false)
-)
+      option.setName('quantidade')
+        .setDescription('Quantidade de warns para remover')
+        .setRequired(true)
+    )
     .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
 
   async execute(interaction) {
+
     const user = interaction.options.getUser('usuario');
-    const numero = interaction.options.getInteger('numero');
-const db = readDB();
-const guildId = interaction.guild.id;
+    const quantidade = interaction.options.getInteger('quantidade');
 
-if (!db.warns[guildId]?.[user.id]) {
-  return interaction.reply('Esse usuário não tem avisos.');
-}
+    const db = readDB();
 
-if (numero) {
-  db.warns[guildId][user.id].splice(numero - 1, 1);
+    if (!db.warns) db.warns = {};
+    if (!db.warns[user.id]) db.warns[user.id] = 0;
 
-  await interaction.reply(`🧹 Warn ${numero} removido de ${user.tag}`);
-} else {
-  db.warns[guildId][user.id] = [];
+    db.warns[user.id] -= quantidade;
 
-  await interaction.reply(`🧹 Todos os warns removidos de ${user.tag}`);
-}
+    if (db.warns[user.id] < 0) db.warns[user.id] = 0;
 
-writeDB(db);
+    writeDB(db);
+
+    return interaction.reply({
+      content: `⚠️ Foram removidos ${quantidade} warn(s) de ${user.tag}. Agora ele tem ${db.warns[user.id]} warn(s).`
+    });
   }
 };
