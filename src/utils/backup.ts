044ce5ab3a8fe71506513589ffc8f32cd9a11881
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-22 23:10:05 -0300

    Update backup.ts

--- src/utils/backup.ts
+++ src/utils/backup.ts
@@ -1,16 +1,30 @@
 import fs from "fs";
 import GuildConfig from "../models/GuildConfig.js";
 
-export async function backupDatabase() {
+const OWNER_ID = "838197254313476147";
+
+export async function backupDatabase(client) {
   try {
     const data = await GuildConfig.find();
 
-    const backup = JSON.stringify(data, null, 2);
+    const fileName = `backup-${Date.now()}.json`;
+    const filePath = `./${fileName}`;
 
-    fs.writeFileSync(`./backup.json`, backup);
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 
-    console.log("💾 Backup salvo com sucesso");
+    console.log("💾 Backup salvo");
+
+    // 🔥 envia no privado
+    const user = await client.users.fetch(OWNER_ID);
+
+    if (user) {
+      await user.send({
+        content: "💾 Novo backup do bot:",
+        files: [filePath]
+      });
+    }
+
   } catch (err) {
-    console.error("❌ Erro ao fazer backup:", err);
+    console.error("❌ Erro no backup:", err);
   }
 }
