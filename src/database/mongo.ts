be351259d116c60626e65364f286d508062bd549
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-24 17:24:29 -0300

    Update mongo.ts

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -2,19 +2,11 @@ import mongoose from "mongoose";
 
 export async function connectMongo() {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("❌ MONGO_URI não definida");
-    }
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("🔄 Conectando ao MongoDB...");
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000 // evita travar 10s
-    });
-
-    console.log("✅ MongoDB conectado com sucesso!");
+    console.log("✅ MongoDB conectado");
   } catch (err) {
     console.error("❌ Erro Mongo:", err);
-    process.exit(1); // 🔥 impede o bot de rodar quebrado
+    process.exit(1);
   }
 }
