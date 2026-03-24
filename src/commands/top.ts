ffb617249739f28c0e1489810da8d227edb9c3f0
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-24 17:47:55 -0300

    Update top.ts

--- src/commands/top.ts
+++ src/commands/top.ts
@@ -3,49 +3,15 @@ import {
   EmbedBuilder
 } from "discord.js";
 
-import GuildConfig from "../models/GuildConfig.js";
-
 export default {
   data: new SlashCommandBuilder()
     .setName("top")
-    .setDescription("Ranking de staff"),
+    .setDescription("Ranking de avaliações"),
 
   async execute(interaction) {
-    const guild = await GuildConfig.findOne({
-      guildId: interaction.guild.id
-    });
-
-    if (!guild || !guild.avaliacoes.length) {
-      return interaction.reply("Sem avaliações.");
-    }
-
-    const stats = {};
-
-    guild.avaliacoes.forEach(av => {
-      if (!stats[av.user]) {
-        stats[av.user] = { total: 0, soma: 0 };
-      }
-
-      stats[av.user].total++;
-      stats[av.user].soma += parseInt(av.estrelas);
-    });
-
-    const ranking = Object.entries(stats)
-      .map(([user, s]: any) => ({
-        user,
-        media: (s.soma / s.total).toFixed(2),
-        total: s.total
-      }))
-      .sort((a, b) => b.media - a.media)
-      .slice(0, 10);
-
-    const text = ranking.map((u, i) =>
-      `${i + 1}. ${u.user}\n⭐ ${u.media} (${u.total})`
-    ).join("\n\n");
-
     const embed = new EmbedBuilder()
-      .setTitle("🏆 Ranking")
-      .setDescription(text)
+      .setTitle("🏆 Top usuários")
+      .setDescription("Ranking ainda em desenvolvimento.")
       .setColor("Gold");
 
     await interaction.reply({ embeds: [embed] });
