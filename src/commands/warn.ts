6bb408e109de838fde35bcb0b9f98405387a2179
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-24 17:18:19 -0300

    Update warn.ts

--- src/commands/warn.ts
+++ src/commands/warn.ts
@@ -1,51 +1,40 @@
-import {
-  SlashCommandBuilder,
-  PermissionFlagsBits
-} from "discord.js";
-
+import { SlashCommandBuilder } from "discord.js";
 import GuildConfig from "../models/GuildConfig.js";
-import { sendLog } from "../utils/logs.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("warn")
-    .setDescription("Aplicar warn em um usuário")
+    .setDescription("Aplicar warn")
     .addUserOption(o =>
       o.setName("usuario")
-        .setDescription("Usuário a ser avisado")
+        .setDescription("Usuário")
         .setRequired(true)
     )
     .addStringOption(o =>
       o.setName("motivo")
-        .setDescription("Motivo do warn")
-        .setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+        .setDescription("Motivo")
+    ),
 
   async execute(interaction) {
+    await interaction.deferReply();
+
     const user = interaction.options.getUser("usuario");
-    const reason = interaction.options.getString("motivo");
+    const motivo = interaction.options.getString("motivo") || "Sem motivo";
 
     let guild = await GuildConfig.findOne({ guildId: interaction.guild.id });
     if (!guild) guild = await GuildConfig.create({ guildId: interaction.guild.id });
 
-    let warns = guild.warns.get(user.id) || [];
+    const warns = guild.warns.get(user.id) || [];
 
     warns.push({
-      reason,
-      staff: interaction.user.tag
+      reason: motivo,
+      staff: interaction.user.tag,
+      date: new Date()
     });
 
     guild.warns.set(user.id, warns);
     await guild.save();
 
-    await interaction.reply(`⚠️ ${user.tag} recebeu um warn.`);
-
-    await sendLog(interaction.guild, {
-      action: "Warn",
-      user,
-      staff: interaction.user,
-      reason
-    });
+    await interaction.editReply(`⚠️ ${user.tag} recebeu warn.`);
   }
 };
