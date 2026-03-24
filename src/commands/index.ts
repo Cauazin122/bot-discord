a25faf2728b96c1ca0f1cd151569e3dac063bdcc
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-24 17:16:59 -0300

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,28 +1,22 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
-import * as ping from "./ping.js";
-import * as ajuda from "./help.js";
-import * as eco from "./echo.js";
-import * as info from "./info.js";
-import * as ticket from "./ticket.js";
-import * as avaliacoes from "./avaliacoes.js";
-import * as top from "./top.js";
+import ping from "./ping.js";
+import ajuda from "./help.js";
+import eco from "./echo.js";
+import info from "./info.js";
+import ticket from "./ticket.js";
+import avaliacoes from "./avaliacoes.js";
+import top from "./top.js";
 import ban from "./ban.js";
 import kick from "./kick.js";
 import mute from "./mute.js";
 import warn from "./warn.js";
-import * as helpadm from "./helpadm.js";
+import helpadm from "./helpadm.js";
 import removewarn from "./removewarn.js";
 import warns from "./warns.js";
 import antilink from "./antilink.js";
 import config from "./config.js";
-import automod from "./automod.js";
 
-export interface Command {
-  data: SlashCommandBuilder;
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
-}
-
-export const commands: Record<string, Command> = {
+export const commands = {
   ping,
   ajuda,
   eco,
@@ -38,6 +32,5 @@ export const commands: Record<string, Command> = {
   removewarn,
   warns,
   antilink,
-  config,
-  automod
+  config
 };
