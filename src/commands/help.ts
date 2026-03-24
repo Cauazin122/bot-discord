a7c519bed3621be0dfa293633740b5fca76943af
Cauazin122 <cauahenriquedossantos00@gmail.com>
2026-03-24 17:52:58 -0300

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -11,7 +11,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     .setDescription("Aqui estão todos os comandos que você pode usar:")
     .addFields(
       { name: "/ping", value: "Verifica a latência do bot", inline: true },
-      { name: "/ajuda", value: "Mostra esta mensagem de ajuda", inline: true },
+      { name: "/help", value: "Mostra esta mensagem de ajuda", inline: true },
       { name: "/eco", value: "Repete uma mensagem para você", inline: true },
       { name: "/info", value: "Mostra informações sobre o servidor", inline: true },
       { name: "/warns", value: "Comando usado para ver as warns de um membro ou as suas",inline: true }
